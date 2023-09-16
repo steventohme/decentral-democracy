ee67c0f1f0c635d62cef7f12ef6d6b7d0a943a61
Steven Tohme <steventohme59@gmail.com>
2023-09-16 14:45:03 -0400

    update vote to use simpler encryption

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 
 contract VotingSystem {
     address[] public voters;
     mapping(address => bool) public isRegistered;
     mapping(address => bool) public hasVoted;
+    mapping(address => bytes) public encryptedVotes;
 
     event Voted(address indexed voter, uint indexed candidateId);
     event DebugLog(string message, address indexed voterAddress);
-
-    using ECDSA for bytes32;
-    mapping(address => bytes) public encryptedVotes;
+    
 
     struct Candidate {
         uint id;
@@ -43,11 +41,12 @@ contract VotingSystem {
         require(isRegistered[msg.sender], "Voter is not registered.");
         require(!hasVoted[msg.sender], "Voter has already voted.");
 
-        bytes32 encryptionKey =  keccak256(abi.encodePacked(block.timestamp, msg.sender));
-        bytes memory encrypted = ECDSA.secp256k1Encrypt(encryptedVote, encryptionKey);
-
+        bytes memory key = bytes("secret");
+        bytes memory encrypted = new bytes(encryptedVote.length);
+        for (uint i = 0; i < encryptedVote.length; i++) {
+            encrypted[i] = encryptedVote[i] ^ key[i % key.length];
+        }
         encryptedVotes[msg.sender] = encrypted;
-
         hasVoted[msg.sender] = true;
     }
 }
