12256e9c23c9bbd71d81c55c7bf54b2fc8d1621c
Steven Tohme <steventohme59@gmail.com>
2023-09-16 02:05:37 -0400

    create simple contract

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.8.0;
 contract VotingSystem {
     address[] public voters;
     mapping(address => bool) public hasVoted;
+
+    struct Candidate {
+        uint id;
+        string name;
+        string affiliation;
+        uint voteCount;
+    }
+
+    Candidate[] public candidates;
+
+    constructor(string[] memory candidateNames, string[] memory candidateAffiliations) {
+        require(candidateNames.length == candidateAffiliations.length, "Mismatch between names and affiliations.");
+        for (uint i = 0; i < candidateNames.length; i++) {
+            candidates.push(Candidate(i, candidateNames[i], candidateAffiliations[i], 0));
+        }
+    }
+
 }
