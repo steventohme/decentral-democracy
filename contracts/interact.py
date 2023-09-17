e9c85aaa45b9ea2587d94656f27525c0c2c52048
Steven Tohme <steventohme59@gmail.com>
2023-09-17 00:56:40 -0400

    bug fixes in interact.py

--- contracts/interact.py
+++ contracts/interact.py
@@ -1,19 +1,11 @@
 from web3 import Web3, HTTPProvider, contract
 import json
-
 # Connect to a local Ethereum node (replace with your node's URL)
 w3 = Web3(HTTPProvider("http://127.0.0.1:7545"))
 
-# Ensure the connection is successful
-if not w3.isConnected():
-    print("Failed to connect to the Ethereum node.")
-    exit()
-
-print("Connected to Ethereum node")
-
 # Replace with the path to your contract's JSON ABI file and contract address
 contract_abi_path = '../build/contracts/VotingSystem.json'
-contract_address = '0x30EAa006eF9D1BC0333abA61A4e94aB442106DAd'
+contract_address = '0x28DCa635551fd11b6b070a5DFBC5DCa1763f78e4'
 
 with open(contract_abi_path) as f:
     contract_abi = json.load(f)["abi"]
@@ -38,9 +30,9 @@ def main():
     has_voted_status = voting_system_contract.functions.hasVoted(voter_address).call()
     print('Has Voted:', has_voted_status)
 
-    print('Register Voter Receipt Status:', w3.eth.waitForTransactionReceipt(register_voter_receipt)['status'])
-    print('Vote Receipt Status:', w3.eth.waitForTransactionReceipt(vote_receipt)['status'])
 
+    print('Register Voter Receipt Status:', has_voted_status)
+    print('Vote Receipt Status:', vote_receipt)
     print('Voter registered and vote cast successfully.')
 
 if __name__ == '__main__':
